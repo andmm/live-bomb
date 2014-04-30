f2d391a04a519180ab1b0671ad972d2ff4d8033b
Chase Pettit <chasepettit@gmail.com>
2014-04-30 18:39:44 -0400

    Updates to circumvent YQL issues.

--- functions.js
+++ functions.js
@@ -1,11 +1,7 @@
 //Live check query variables.
 //Variables
 
-var gbSchedule_url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.giantbomb.com%22%20and%20compat%3D%22html5%22%20and%20xpath%3D'%2F%2Fdl%5Bcontains(%40class%2C%22promo-upcoming%22)%5D'&format=json&callback=";
-var gb_url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.giantbomb.com%22%20and%20compat%3D%22html5%22%20and%20xpath%3D'%2F%2Fspan%5Bcontains(%40class%2C%22header-promo%20live%20show%22)%5D'&format=json&callback=";
-var gbPromo_url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.giantbomb.com%22%20and%20compat%3D%22html5%22%20and%20xpath%3D'%2F%2Fdiv%5Bcontains(%40class%2C%22kubrick-promo-video%22)%5D'&format=json&callback=";
-var chat_url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.giantbomb.com%2Fchat%22%20and%20xpath%3D'%2Fhtml%2Fhead%2Ftitle'&format=json&callback=";
-var premiumImage_url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.giantbomb.com%22%20and%20compat%3D%22html5%22%20and%20xpath%3D'%2F%2F*%5B%40id%3D%22wrapper%22%5D'&format=json&callback=";
+var gb_url = "https://query.yahooapis.com/v1/public/yql?q=select%20content%20from%20data.headers%20where%20url%3D%22http%3A%2F%2Fwww.giantbomb.com%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
 var gbLive;
 var storage = $.localStorage;
 var scheduleLoadingIcon = $('#lb-schedule-loading');
@@ -17,10 +13,12 @@ var scheduleCounter;
 var checkLive = function(){
 	var checkLiveDone = $.Deferred();
 
-		$.getJSON (gb_url, function(data){	
-			var checkForResults = data.query.results; 
-		if (checkForResults == null) {
-			gbLive = false; 	
+	$.getJSON (gb_url, function(data){
+		var parsedHtml = '<body>' + data.query.results.resources.content.replace(/^[\s\S]*<body.*?>|<\/body>[\s\S]*$/g, '') + '</body>';
+		var parsedElem = $(parsedHtml).find('.header-promo.live.show');
+
+		if (parsedElem.length == 0) {
+			gbLive = false;
 			console.log('checkLive()');
 			storage.set({
 				'islive':false,
@@ -28,10 +26,10 @@ var checkLive = function(){
 			});
 			checkLiveDone.resolve();
 		} else {
-		var checkForTimer = data.query.results.span.p.content;	
-			if (checkForTimer == undefined) {
+			var liveLink = $(parsedElem).find('p a');
+			if (liveLink.length > 0) {
 				gbLive = true;
-				var titleName = data.query.results.span.p.a.content.replace(/[<>]/,'');
+				var titleName = $(liveLink).text().replace(/[<>]/,'');
 				storage.set({
 					'islive': true,
 					'title': titleName,
@@ -40,8 +38,8 @@ var checkLive = function(){
 				getShowImage();
 				console.log('checkLive()');
 				checkLiveDone.resolve();
-			   	} else {
-				gbLive = false; 
+			} else {
+				gbLive = false;
 				storage.set({
 					'islive': false,
 					'counter':true,
@@ -49,10 +47,10 @@ var checkLive = function(){
 				});
 				console.log('checkLive()');
 				checkLiveDone.resolve();
-						
-					}
-				} 
-			}); 
+
+			}
+		}
+	});
 	return checkLiveDone.promise();
 };
 
@@ -62,200 +60,115 @@ var getSchedule = function(){
 
 	var getScheduleDone = $.Deferred();
 
-	$.getJSON (gbSchedule_url, function(data){	
-		
+	$.getJSON (gb_url, function(data){
+
 		scheduleCounter = 0;
 
 		if (data.query.count > 0) {
 
-			if (data.query.results.dl.dd.length > 1) {
+			var parsedHtml = '<body>' + data.query.results.resources.content.replace(/^[\s\S]*<body.*?>|<\/body>[\s\S]*$/g, '') + '</body>';
+			var parsedElem = $(parsedHtml).find('dl.promo-upcoming').children('dd');
+
+			if (parsedElem.length > 0) {
 				var output = "<ul>";
-				$.each(data.query.results.dl.dd, function(index, value){
-					var imgUrl = value.style.substring(21).replace(')','');	
-					var eventName = value.div.h4.content.replace(/[<>]/,'');
-					var eventTypeFull = value.div.p.content.length;
-					
-					switch(eventTypeFull)
-					{
-					//Live Show
-						case 34:
+				$.each(parsedElem, function(index, value){
+					var imgUrl = $(value).css('background-image').replace('url(','').replace(')','');
+					var eventName = $(value).find('h4').text().replace(/[<>]/,'');
+					var eventInfo = $(value).find('p').text();
+
+					// Parse Event Type and Date/Time
+					if (eventInfo.indexOf('Live Show') == 0) {
 						var eventType = '<i class="fa fa-dot-circle-o fa-lg circle-schedule"></i> Live Show on ';
-						var yqlDate = value.div.p.content.substring(13);
-						break;
-						
-					//Video
-						case 30: 
+						var yqlDate = eventInfo.substring(13);
+					} else if (eventInfo.indexOf('Video') == 0) {
 						var eventType = '<i class="fa fa-play-circle fa-lg circle-schedule"></i> Video on ';
-						var yqlDate = value.div.p.content.substring(9);
-						break;
-					//Article/Podcast
-						default: 
-						var scheduleText = value.div.p.content;
-						var t = scheduleText.substring(0,7);
-						if (t == "Article") {
-							var eventType = '<i class="fa fa-file-o fa-lg"></i> Article on ';
-						} else { 
-							var eventType = '<i class="fa fa-microphone fa-lg"></i> Podcast on ';
-						}	
-						var yqlDate = value.div.p.content.substring(11);	
+						var yqlDate = eventInfo.substring(9);
+					} else if (eventInfo.indexOf('Article') == 0) {
+						var eventType = '<i class="fa fa-file-o fa-lg"></i> Article on ';
+						var yqlDate = eventInfo.substring(11);
+					} else {
+						var eventType = '<i class="fa fa-microphone fa-lg"></i> Podcast on ';
+						var yqlDate = eventInfo.substring(11);
 					}
-						var m = ['0','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-						var dateString = ''+yqlDate.substring(8,12)+''+'/0'+m.indexOf(''+yqlDate.substring(0,3)+'')+'/'+''+yqlDate.substring(4,6)+''+' '+
-						''+yqlDate.substring(13,18)+' '+''+yqlDate.substring(19)+''+' EDT';
-						var dt = new Date(''+dateString+'');
-						var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-						var eventDate = months[dt.getMonth()]+' '+dt.getDate()+', '+dt.getFullYear()+' '+dt.toLocaleTimeString().replace(/:\d{2}\s/,' ');
-						output +=  '<li style="background-image: url('+ imgUrl +')" class="animated fadeInDownBig"><h4>' + eventName + 
-						'</h4> <p class="lb-schedule-p">'+eventType+ eventDate+'</p></li>';
-					scheduleCounter += 1;	
-			});	
-			output += "</ul>";
-			$('#lb-schedule-items').html(output);
-			getScheduleDone.resolve();
-			
-			//One item on the schedule
-			} else if (data.query.results.dl.dd.length == 1 || data.query.count == 1 ) {
-				var imgUrl = data.query.results.dl.dd.style.substring(21).replace(')','');
-				var eventName =  data.query.results.dl.dd.div.h4.content.replace(/[<>]/,'');
-				var eventTypeFull = data.query.results.dl.dd.div.p.content.length;
-				switch(eventTypeFull)
-				{
-				//Live show
-					case 34:  
-						var eventType = '<i class="fa fa-dot-circle-o fa-lg circle-schedule"></i> Live Show on ';	
-						var yqlDate = data.query.results.dl.dd.div.p.content.substring(13);
-						console.log(yqlDate);
-						break;
-				//Video
-					case 30: 
-						var eventType = '<i class="fa fa-play-circle fa-lg"></i> Video on ';
-						var yqlDate = data.query.results.dl.dd.div.p.content.substring(9);		
-						break;
-				//Article/Podcast
-					default:
-						var scheduleText = data.query.results.dl.dd.div.p.content;
-						var t = scheduleText.substring(0,7);
-						if (t == "Article") {
-							var eventType = '<i class="fa fa-file-o fa-lg"></i> Article on ';
-						} else {
-							var eventType = '<i class="fa fa-microphone fa-lg"></i> Podcast on ';
-						}
-					var yqlDate = data.query.results.dl.dd.div.p.content.substring(11);	
-				}
-				var m = ['0','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-				var dateString = ''+yqlDate.substring(8,12)+''+'/0'+m.indexOf(''+yqlDate.substring(0,3)+'')+'/'+''+yqlDate.substring(4,6)+''+' '+
-				''+yqlDate.substring(13,18)+' '+''+yqlDate.substring(19)+''+' EDT';
-				var dt = new Date(''+dateString+'');
-				var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-				var eventDate = months[dt.getMonth()]+' '+dt.getDate()+', '+dt.getFullYear()+' '+dt.toLocaleTimeString().replace(/:\d{2}\s/,' ');
-				var output =  '<ul><li style="background-image: url('+ imgUrl +')" class="animated fadeInDownBig"><h4>' + eventName + 
-				'</h4> <p class="lb-schedule-p">'+eventType+ eventDate+'</p></li></ul>';
-				scheduleCounter = 1;
+
+					var m = ['0','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+					var dateString = ''+yqlDate.substring(8,12)+''+'/0'+m.indexOf(''+yqlDate.substring(0,3)+'')+'/'+''+yqlDate.substring(4,6)+''+' '+
+					''+yqlDate.substring(13,18)+' '+''+yqlDate.substring(19)+''+' EDT';
+					var dt = new Date(''+dateString+'');
+					var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+					var eventDate = months[dt.getMonth()]+' '+dt.getDate()+', '+dt.getFullYear()+' '+dt.toLocaleTimeString().replace(/:\d{2}\s/,' ');
+					output +=  '<li style="background-image: url('+ imgUrl +')" class="animated fadeInDownBig"><h4>' + eventName +
+					'</h4> <p class="lb-schedule-p">'+eventType+ eventDate+'</p></li>';
+
+					scheduleCounter += 1;
+				});
+
+				output += "</ul>";
 				$('#lb-schedule-items').html(output);
 				getScheduleDone.resolve();
+
 			} else {
 				$('#lb-schedule-items').html('<h5 id="no-schedule">There are no items on the schedule. Try refreshing</h5>');
 				scheduleCounter = 0;
 				getScheduleDone.resolve();
-				
 			}
 		} else {
 			$('#lb-schedule-items').html('<h5 id="no-schedule" class="animated slideInDown">There are no items on the schedule or something went wrong. Try refreshing</h5>');
 			scheduleCounter = 0;
 			getScheduleDone.resolve();
-	} 
+		}
 
 	});
 
 	return getScheduleDone.promise();
-};	
+};
 
 
 
 var getShowImage = function(){
 
-var getShowImageDone = $.Deferred();
-		
+	var getShowImageDone = $.Deferred();
+
 	if (storage.isSet('image') == false) {
 
-	 $.getJSON(gb_url,function(liveshow){
-	 	if ( liveshow.query.count < 1 ) {
-	 		$('#lb-status-live').css('background-image','url(/images/premium-background.png)');
-			getShowImageDone.resolve();
-		} else {
-	 	var checkForVideo = liveshow.query.results.span.p.content;
-	
-	 	if (checkForVideo == undefined) { 
-	 		$.getJSON(gbPromo_url, function(data){
-	 			console.log(data);
-			if ( data.query.count > 0 && data.query.results.div.style != undefined) {
-				var promoUrlFull = data.query.results.div.style;
-				var promoUrl = promoUrlFull.substring(22).replace(')','');
-				$('#lb-status-live').css('background-image','url('+promoUrl+')');
-			 	getShowImageDone.resolve();			
-			} else if (data.query.results.div.style == undefined) {
-				$.getJSON(premiumImage_url,function(data){
-					var promoImages = data.query.results.div.section.div.ul.li;
-					var premiumImage = promoImages[0].style;
-					var premiumImageUrl = premiumImage.substring(22).replace(')','');
-					$('#lb-status-live').css('background-image','url('+premiumImageUrl+')');
-					getShowImageDone.resolve();	
-					});	
-				}
-			});
-	 	} else {
+		$.getJSON(gb_url,function(liveshow){
+			if ( liveshow.query.count < 1 ) {
+				$('#lb-status-live').css('background-image','url(/images/premium-background.png)');
+				getShowImageDone.resolve();
+			} else {
+				var checkForVideo = liveshow.query.results.span.p.content;
 
-			$('#lb-status-live').css('background-image','url(/images/premium-background.png)');
-			getShowImageDone.resolve();
+				if (checkForVideo == undefined) {
+					$.getJSON(gbPromo_url, function(data){
+
+						if (data.query.count > 0) {
+							var parsedHtml = '<body>' + data.query.results.resources.content.replace(/^[\s\S]*<body.*?>|<\/body>[\s\S]*$/g, '') + '</body>';
+							var parsedElem = $(parsedHtml).find('.kubrick-promo-video');
+
+							if ($(parsedElem).css('background-image') != '') {
+								var backgroundImage = $(parsedElem).css('background-image');
+								$('#lb-status-live').css('background-image',backgroundImage);
+								getShowImageDone.resolve();
+							} else {
+								parsedElem = $(parsedHtml).find('#wrapper section.promo-strip div ul li').eq(0);
+								var backgroundImage = $(parsedElem).css('background-image');
+								$('#lb-status-live').css('background-image',backgroundImage);
+								getShowImageDone.resolve();
+							}
+						}
+					});
+				} else {
+
+					$('#lb-status-live').css('background-image','url(/images/premium-background.png)');
+					getShowImageDone.resolve();
 				}
 			}
-		});		
-	
+		});
+
 	} else {
 		$('#lb-status-live').css('background-image','url('+storage.get('image')+')');
 		getShowImageDone.resolve();
 	}
-				
+
 	return getShowImageDone.promise();
-};	
-
-
-//var getShowImageNew = function(){
-//
-//var getShowImageDone = $.Deferred();
-//		
-//	if (storage.isSet('image') == false) {
-//
-//		$.getJSON(gbPromo_url, function(data){
-//			if ( data.query.count > 0 && data.query.results.div.style != undefined) {
-//				var promoUrlFull = data.query.results.div.style;
-//				var promoUrl = promoUrlFull.substring(22).replace(')','');
-//				$('#lb-status-live').css('background-image','url('+promoUrl+')');
-//				storage.set('image',''+promoUrl+'');
-//			 	getShowImageDone.resolve();			
-//			} else if (data.query.results.div.style == undefined) {
-//				$.getJSON(premiumImage_url,function(data){
-//					var promoImages = data.query.results.div.section.div.ul.li;
-//					var premiumImage = promoImages[0].style;
-//					var premiumImageUrl = premiumImage.substring(22).replace(')','');
-//					$('#lb-status-live').css('background-image','url('+premiumImageUrl+')');
-//					//storage.set('image',''+premiumImageUrl+'');
-//					getShowImageDone.resolve();	
-//				});	
-//			} else {
-//			$('#lb-status-live').css('background-image','url(/images/premium-background.png)');
-//			getShowImageDone.resolve();
-//			}
-//		});
-//	} else {
-//		$('#lb-status-live').css('background-image','url('+storage.get('image')+')');
-//		getShowImageDone.resolve();
-//	}
-//		
-//		
-//	return getShowImageDone.promise();
-//};	
-
-
-
-
+};
