635fad337d144abf15ec96bb00cc1af9e75e6fa1
Paul Friedman <paul.friedman@cbsinteractive.com>
2014-07-11 14:52:24 -0700

    Normalize JavaScript to a consistent style

--- js/background.js
+++ js/background.js
@@ -1,8 +1,8 @@
 
 //Default Settings
-if (storage.isSet('preferences') == false) {
+if (storage.isSet('preferences') === false) {
     storage.set({
-        'preferences':'default',
+        'preferences': 'default',
         'notification': true,
         'timer': 300000,
         'theme': 'dark',
@@ -11,75 +11,76 @@ if (storage.isSet('preferences') == false) {
         'schedule-bagde': true,
         'show-schedule': true
     });
-    scheduleCounter = 0;
-};
 
-if (storage.isSet('show-schedule') == false){
-    storage.set('show-schedule',true);
+    scheduleCounter = 0;
 }
 
+if (storage.isSet('show-schedule') === false){
+    storage.set('show-schedule', true);
+}
 
 $.ionSound({
     sounds: [
-    "bumper",
-    "rapman",
-    "dropbomb",
-    "bman"
+        'bumper',
+        'rapman',
+        'dropbomb',
+        'bman'
     ],
-    path: "/sounds/",
+    path: '/sounds/',
     multiPlay: true
 });
 
 //GA
-ga('send','pageview','/running');
+ga('send', 'pageview', '/running');
 
 //Routine
-var liveRoutine = function(){
+var liveRoutine = function() {
 
-    checkLive().done(function(){
+    checkLive().done(function() {
         var options = {
-            type:"basic",
-            title:"Hey duder! GB is live with:",
-            message:""+storage.get('title')+"",
-            iconUrl:"/images/notificationicon.png"
-        }
+            type: 'basic',
+            title: 'Hey duder! GB is live with:',
+            message: '' + storage.get('title') + '',
+            iconUrl: '/images/notificationicon.png'
+        };
 
-        if (gbLive == true) {
-            chrome.browserAction.setBadgeText({text:'LIVE'});
+        if (gbLive === true) {
+            chrome.browserAction.setBadgeText({ text: 'LIVE' });
             $('body').trigger('statusLive');
-            if (storage.get('notification') == true && sendMessage == true) {
+
+            if (storage.get('notification') === true && sendMessage === true) {
                 sendMessage = false;
                 chrome.notifications.create('notify', options, function(){});
-                if ( storage.get('notification-sound') == true ) {
-                    $.ionSound.play(''+storage.get('sound')+'');
+
+                if (storage.get('notification-sound') === true) {
+                    $.ionSound.play('' + storage.get('sound') + '');
                 }
             }
         } else {
             $('body').trigger('statusNotLive');
+
             sendMessage = true;
-            if ( storage.get('schedule-bagde') == true && scheduleCounter > 0) {
-                chrome.browserAction.setBadgeText({text:''+scheduleCounter+''});
+            if (storage.get('schedule-bagde') === true && scheduleCounter > 0) {
+                chrome.browserAction.setBadgeText({text: '' + scheduleCounter + ''});
             } else {
-                chrome.browserAction.setBadgeText({text:''});
+                chrome.browserAction.setBadgeText({text: ''});
             }
         }
     });
 };
 
-var scheduleRoutine = function(){
-
+var scheduleRoutine = function() {
     window.buttonRefreshSchedule.addClass('hidden');
 
-    getSchedule().done(function(){
-        if (window.scheduleLoadingIcon.length > 0)
-        {
-            window.scheduleLoadingIcon.effect('fadeOut',2000, function(){
+    getSchedule().done(function() {
+        if (window.scheduleLoadingIcon.length > 0) {
+            window.scheduleLoadingIcon.effect('fadeOut', 2000, function() {
                 $(this).fadeOut(300);
             });
         }
 
-        if ( storage.get('schedule-bagde') == true && gbLive == false && scheduleCounter > 0) {
-            chrome.browserAction.setBadgeText({text:''+scheduleCounter+''});
+        if (storage.get('schedule-bagde') === true && gbLive === false && scheduleCounter > 0) {
+            chrome.browserAction.setBadgeText({text: '' + scheduleCounter + ''});
         }
 
         if (window.buttonRefreshSchedule.length > 0)
@@ -92,20 +93,18 @@ var scheduleRoutine = function(){
 liveRoutine();
 scheduleRoutine();
 
-//Click on notification
-chrome.notifications.onClicked.addListener(function(notify){
-    $.ionSound.stop(''+storage.get('sound')+'');
+// Click on notification
+chrome.notifications.onClicked.addListener(function(notify) {
+    $.ionSound.stop('' + storage.get('sound') + '');
     chrome.tabs.create({
         url: 'http://www.giantbomb.com/chat'
     });
 });
 
-//Close notifications
-chrome.notifications.onClosed.addListener(function(notify){
-    $.ionSound.stop(''+storage.get('sound')+'');
+// Close notifications
+chrome.notifications.onClosed.addListener(function(notify) {
+    $.ionSound.stop('' + storage.get('sound') + '');
 });
 
-
 setInterval(scheduleRoutine, 600000);
-
 setInterval(liveRoutine, storage.get('timer'));
--- js/functions.js
+++ js/functions.js
@@ -11,28 +11,27 @@ var scheduleLoadingIcon = $('#lb-schedule-loading');
 var buttonRefreshSchedule = $('#lb-refresh-schedule');
 
 //Check for live video
-var checkLive = function(){
+var checkLive = function() {
     var checkLiveDone = $.Deferred();
 
     $.getJSON(gb_url, function(data){
-        if (data.liveNow != null)
-        {
+        if (data.liveNow !== null) {
             gbLive = true;
+
             storage.set({
                 'islive': true,
                 'title': data.liveNow.title,
                 'liveImage': data.liveNow.image,
-                'counter':false
+                'counter': false
             });
 
             checkLiveDone.resolve();
-        }
-        else
-        {
+        } else {
             gbLive = false;
+
             storage.set({
-                'islive':false,
-                'counter':false
+                'islive': false,
+                'counter': false
             });
 
             checkLiveDone.resolve();
@@ -44,47 +43,43 @@ var checkLive = function(){
 
 
 //Schedule Function
-var getSchedule = function(){
+var getSchedule = function() {
 
     var getScheduleDone = $.Deferred();
 
-    $.getJSON(gb_url, function(data){
-
+    $.getJSON(gb_url, function(data) {
         scheduleCounter = 0;
 
         if (data.upcoming.length > 0) {
-            var output = "<ul>";
-            $.each(data.upcoming, function(key,val) {
+            var output = "<ul>",
+                eventType,
+                scheduleDate;
+
+            $.each(data.upcoming, function(key, val) {
                 // Parse Event Type
-                switch (val.type)
-                {
-                case "Live Show":
-                    var eventType = '<i class="fa fa-dot-circle-o fa-lg circle-schedule"></i> Live Show ';
-                    break;
-                case "Video":
-                    var eventType = '<i class="fa fa-play-circle fa-lg circle-schedule"></i> Video ';
-                    break;
-                case "Article":
-                    var eventType = '<i class="fa fa-file-o fa-lg"></i> Article ';
-                    break;
-                case "Podcast":
-                    var eventType = '<i class="fa fa-microphone fa-lg"></i> Podcast ';
-                    break;
+                switch (val.type) {
+                case 'Live Show':
+                    eventType = '<i class="fa fa-dot-circle-o fa-lg circle-schedule"></i> Live Show '; break;
+                case 'Video':
+                    eventType = '<i class="fa fa-play-circle fa-lg circle-schedule"></i> Video '; break;
+                case 'Article':
+                    eventType = '<i class="fa fa-file-o fa-lg"></i> Article '; break;
+                case 'Podcast':
+                    eventType = '<i class="fa fa-microphone fa-lg"></i> Podcast '; break;
                 default:
-                    var eventType = '<i class="fa fa-question-circle fa-lg"></i> Something ';
-                    break;
+                    eventType = '<i class="fa fa-question-circle fa-lg"></i> Something ';
                 }
 
                 // Parse Date
-                var dt = moment.tz(moment(new Date(val.date)).format("dddd, MMMM Do YYYY, h:mm:ss a"), "dddd, MMMM Do YYYY, h:mm:ss a", "America/Los_Angeles");
+                var dt = moment.tz(moment(new Date(val.date)).format('dddd, MMMM Do YYYY, h:mm:ss a'), 'dddd, MMMM Do YYYY, h:mm:ss a', 'America/Los_Angeles');
                 var today = moment().get('date');
 
                 dt.zone(moment().zone());
 
                 if (today == dt) {
-                    var scheduleDate = dt.fromNow();
+                    scheduleDate = dt.fromNow();
                 } else {
-                    var scheduleDate = "- " + dt.calendar();
+                    scheduleDate = '- ' + dt.calendar();
                 }
 
                 var eventName = val.title;
@@ -92,21 +87,19 @@ var getSchedule = function(){
 
                 // Assemble Output
                 output +=  '<li style="background-image: url('+ eventImage +')" class="animated fadeInDownBig"><h4>' + eventName +
-                '</h4> <p class="lb-schedule-p">'+ eventType + scheduleDate +'</p></li>';
+                    '</h4> <p class="lb-schedule-p">'+ eventType + scheduleDate +'</p></li>';
 
                 scheduleCounter += 1;
             });
 
-            output += "</ul>";
+            output += '</ul>';
             $('#lb-schedule-items').html(output);
             getScheduleDone.resolve();
-
         } else {
             $('#lb-schedule-items').html('<h5 id="no-schedule" class="animated slideInDown">There are no items on the schedule. Try refreshing.</h5>');
             scheduleCounter = 0;
             getScheduleDone.resolve();
         }
-
     });
 
     return getScheduleDone.promise();
